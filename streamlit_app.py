a8cdb77682f1b3620f0628f046905425dedf9e26
unknown <louiecervantes@wvsu.edu.ph>
2023-04-02 07:14:51 +0800

    save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,10 +123,9 @@ def app():
         sizes = list(scounts.values)
         custom_colours = ['#ff7675', '#74b9ff']
 
-        plt.figure(figsize=(12, 4))
+        fig = plt.figure(figsize=(12, 4))
         plt.subplot(1, 2, 1)
-        plt.pie(sizes, labels = labels, textprops={'fontsize': 10}, startangle=140, 
-               autopct='%1.0f%%', colors=custom_colours)
+        plt.pie(sizes, labels = labels, textprops={'fontsize': 10}, startangle=140, autopct='%1.0f%%', colors=custom_colours)
         plt.subplot(1, 2, 2)
         sns.barplot(x = scounts.index, y = scounts.values, palette= 'viridis')
         st.pyplot(fig)
